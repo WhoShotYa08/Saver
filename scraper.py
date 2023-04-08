0d21cd63aae0729ea202ca54a5d335c08fab657b
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-04-08 09:23:34 +0200

    a list of accessable websites

--- scraper.py
+++ scraper.py
@@ -1,17 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+websites = ['https://www.pnp.co.za/pnpstorefront/pnp/en/specials-landing',
+            'https://www.shoprite.co.za/all-xtra-savings',
+            'https://www.woolworths.co.za/cat/Food/Fruit-Vegetables-Salads/_/N-lllnam?No=60&Nrpp=60']
 
 def is_website_online(url):
-    #Confirms whether the website is active
+    '''
+    Confirms whether the website is active
+    '''
     return requests.get(url).ok
 
 
-def identify_website():
+def identify_website(site):
     '''
     Identifies and returns the website name
     '''
-    link = website_resource().url
+    link = website_resource(site).url
     companySite = link.split("/")[2]
 
     return companySite
@@ -21,3 +26,11 @@ def website_resource(website):
     '''Access the website's resources'''
     return requests.get(website)
 
+
+def beautiful_data(site):
+    html_text = BeautifulSoup(website_resource(site).text, 'lxml')
+
+
+for x in range(len(websites)):
+    if is_website_online(websites[x]):
+        print(identify_website(websites[x]))
