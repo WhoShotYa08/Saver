6d19d0ec6024464d639db143c48834dfdc902983
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-04-08 02:19:55 +0200

    function to check whether the website is online

--- scraper.py
+++ scraper.py
@@ -4,6 +4,12 @@ from bs4 import BeautifulSoup
 
 resource = requests.get()
 
+
+def is_website_online(url):
+    #Confirms whether the website is active
+    return requests.get(url).ok
+
+
 def identify_Website():
     '''
     Identifies and returns the website name
@@ -12,4 +18,6 @@ def identify_Website():
     companySite = link.split("/")[2]
 
     return companySite
+
+
 
