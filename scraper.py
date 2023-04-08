def9d1181ad5c874831fabe428a13952312024c3
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-04-08 02:59:10 +0200

    Created a function for the websites resources

--- scraper.py
+++ scraper.py
@@ -2,22 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-resource = requests.get()
-
-
 def is_website_online(url):
     #Confirms whether the website is active
     return requests.get(url).ok
 
 
-def identify_Website():
+def identify_website():
     '''
     Identifies and returns the website name
     '''
-    link = resource.url
+    link = website_resource().url
     companySite = link.split("/")[2]
 
     return companySite
 
 
+def website_resource(website):
+    '''Access the website's resources'''
+    return requests.get(website)
 
