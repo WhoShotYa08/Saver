01dca30e6728e6b2db2ceba11c1bbd150117614c
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-04-25 08:33:02 +0200

    removed used function from password.js

--- Frontend/password.js
+++ Frontend/password.js
@@ -1,28 +1,11 @@
 var passwd = document.getElementById("password");
 
 
-function verifyEmail(){
-    let list = document.getElementsByTagName('li');
-    if(passwd.focus()){
-        for(let i = 0; i < 4; i++){
-            if(includesCaptitals){
-                console.log(list[0].innerHTML);
-                list[0].setAttribute("class", "correct"); 
-            }
-
-            if(includeSpecialChar){
-                console.log(list[1].innerHTML);
-                list[1].setAttribute("class", "correct");
-            }
-        }
-    }
-
-}
 
 function passwdVisbility(){
     let passwdElement = document.getElementById('password');
     let visible = document.getElementById("checkbox");
-    
+
     if (visible.checked){
         passwdElement.type === "text";
     }
