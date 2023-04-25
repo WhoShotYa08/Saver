95d1cec9a8bed7c0569648852fb43d13b58ffb51
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-04-25 10:22:41 +0200

    every other validator is complete only missing the specialValidator:

--- Frontend/password.js
+++ Frontend/password.js
@@ -1,8 +1,14 @@
-var passwd = document.getElementById("password");
+var passwdElement = document.getElementById("password");
+var number = document.getElementById("number");
+var lower = document.getElementById('lower');
+var capital = document.getElementById('capital');
+var special = document.getElementById('special');
+var miniChar = document.getElementById('miniChar');
+var text = document.getElementById("validators");
+
 
 
 function passwdVisbility(){
-    let passwdElement = document.getElementById('password');
     let visible = document.getElementById("checkbox");
 
     if (visible.checked){
@@ -11,32 +17,59 @@ function passwdVisbility(){
     else{
         passwdElement.type === "password";
     }
-
-
 }
 
-function includesCaptitals(){
 
-    return /[A-Z]/.test(passwd.textContent);
-}
+ passwdElement.onkeyup = function(){
+    // checks whether the password includes uppercase letter
+    if(passwdElement.value.match(/[A-Z]/g)) {
+         capital.classList.remove("wrong");
+         capital.classList.add("correct");
+       } else {
+         capital.classList.remove("correct");
+         capital.classList.add("wrong");
+       }
 
-function includeSpecialChar() {
-    const specialChar = ['!@#$%^&*()_+:"<>?/\\']
-    let password = passwd.value;
-
-    for( let x = 0; x < specialChar.length; x++){
-        if(password.includes(specialChar[x])){
-            return true;
-        }
+    // checks whether the password includes lowercase letters
+    if(passwdElement.value.match(/[a-z]/g)) {
+        lower.classList.remove("wrong");
+        lower.classList.add("correct");
+    } else {
+        lower.classList.remove("correct");
+        lower.classList.add("wrong");
     }
-    return false;
+    // checks whether the password includes numbers
+    if(passwdElement.value.match(/[0-9]/g)) {
+        number.classList.remove("wrong");
+        number.classList.add("correct");
+    } else {
+        number.classList.remove("correct");
+        number.classList.add("wrong");
+    }
+
+    // checks whether the password's length is greater or equal to 8
+    if(passwdElement.value.length >= 8) {
+        miniChar.classList.remove("wrong");
+        miniChar.classList.add("correct");
+    } else {
+        miniChar.classList.remove("correct");
+        miniChar.classList.add("wrong");
+    }
+
+    if(passwdElement.value.match(/^[!@#\$%\^\&*\)\(+=._-]+$/g)) {
+        special.classList.remove("wrong");
+        special.classList.add("correct");
+    } else {
+        special.classList.remove("correct");
+        special.classList.add("wrong");
+    }
+ }
+
+//display the password requirements
+passwdElement.onfocus = function(){
+    text.style.display = 'block';
 }
 
-function ListItem() {
-    let li = document.createElement("li");
-
-    return li
+passwdElement.onblur = function(){
+    text.style.display = "none";
 }
-
-
-passwd.addEventListener("focus", verifyEmail());
