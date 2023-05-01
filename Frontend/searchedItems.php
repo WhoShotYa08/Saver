6b512b3a0274b122be17568db59b7d55cfc43078
mabukwabrian <mabukwa25@gmail.com>
2023-05-01 14:02:09 +0200

    Updated searchedItems table

--- Frontend/searchedItems.php
+++ Frontend/searchedItems.php
@@ -1,19 +1,27 @@
 <?php 
     include "dBConnection.php";
     $itemsTable = "CREATE TABLE SearchedItem(
-        CustomerID INT(10) AUTO_INCREMENT PRIMARY KEY,
+        CustomerID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         Mall_Name varchar(50),
         Item_Price decimal(20, 2),
         Discount_Price decimal(20, 2),
         Shop_Name varchar(50),
-        Foreign Key (CustomerID) references signUp_details(CustomerID)
+        CONSTRAINT Foreign Key (CustomerID) references signUp_details(CustomerID)
     )";
 
-    $runtable = mysqli_query($createConnection, $itemsTable);
-    if($runtable==true){
-        echo "successfull CREATION";
-    }
-    else{
-        echo "Failed".mysqli_error($createConnection);
-    }
+    // $runtable = mysqli_query($createConnection, $itemsTable);
+    // if($runtable==true){
+    //     echo "successfull CREATION";
+    // }
+    // else{
+    //     echo "Failed".mysqli_error($createConnection);
+    // }
+
+    // $updateItems = "ALTER TABLE SearchedItem ADD Image_Link varchar(240)";
+    // if(mysqli_query($createConnection, $updateItems)){
+    //     echo "update successful";
+    // }
+    // else{
+    //     echo "update failed";
+    // }
 ?>
