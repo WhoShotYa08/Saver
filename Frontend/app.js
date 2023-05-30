04799925e7207ff25ba66516c9f215677f9f231e
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-05-30 20:46:26 +0200

    google api in progress, still tryna figure out how to pan to current location

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,16 +1,35 @@
- const http = new XMLHttpRequest();
- let result = document.getElementById("result");
+const http = new XMLHttpRequest();
+let result = document.getElementById("result");
+var coordinates = new Object();
 
- document.getElementById("share").addEventListener("click",()=>{
+
+async function initMap(){
+      //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -28.479, lng: 24.672 },
+    zoom: 6, 
+  });
+  let infoWindow = new google.maps.InfoWindow();
+  document.getElementById("share").addEventListener("click",()=>{
     findCurrentPosition();
- })
+  });
+
+  infoWindow.setPosition(coordinates);
+  infoWindow.setContent("Location found.");
+  infoWindow.open(map);
+  map.setCenter(coordinates);
+
+}
 
 function findCurrentPosition(){
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(
             (position)=> {
-                console.log(position.coords.latitude, position.coords.longitude);
+                coordinates["latitude"] = position.coords.latitude;
+                coordinates["longitude"] = position.coords.longitude;
             },
             (err)=>{
                 alert(err.message);
@@ -20,3 +39,6 @@ function findCurrentPosition(){
         alert("Geolocation not supported by browser");
     }
 }
+
+
+initMap();
